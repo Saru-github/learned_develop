f1dfabf5fe0bbb21433c55c90926ce7c6a371a1d
Saru <heo_dark@naver.com>
2022-01-27 09:43:35 +0900

    Day 1 / 2022.01.25

---
+++ chap01/src/exam01/SystemOutPrintln.java
@@ -0,0 +1,24 @@
+package exam01;
+
+public class SystemOutPrintln {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java!");
+
+		System.out.println(100);
+		System.out.println(3.14);
+
+		System.out.println(100 + 15);
+		System.out.println(100 - 15);
+		System.out.println(100 * 15);
+		System.out.println(100 / 15);
+		
+		System.out.println("Hello" + "Java!");
+		
+		System.out.println("100" + 100);
+		
+
+	}
+
+}
