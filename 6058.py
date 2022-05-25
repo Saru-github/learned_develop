9a9b4173d240b97f32df0cdce4c94f1d37336674
Saru <heo_dark@naver.com>
2022-05-25 09:38:59 +0900

    20220525

--- 6058.py
+++ 6058.py
@@ -5,12 +5,13 @@
 # 2개의 정수가 공백을 두고 입력된다.
 
 # 출력
-# 두 값의 True / False 값이 서로 다를 경우만 True 를 출력하고, 그 외의 경우에는 False 를 출력한다.
+# 두 값의 True / False 값이 모두 False 일 때만 True 를 출력하고, 그 외의 경우에는 False 를 출력한다.
+
 
 a,b = input().split()
 a = bool(int(a))
 b = bool(int(b))
-if a != b or b != a:
+if a == 0 and b == 0:
     print('True')
 else:
     print('False')
---
+++ 6069.py
@@ -0,0 +1,30 @@
+
+# 6069
+
+# 입력
+# 영문자 1개가 입력된다.
+# (A, B, C, D 등 문자 1개가 입력된다.)
+#
+# 출력
+# 문자에 따라 다른 내용이 출력된다.
+
+# 평가 내용
+# 평가 : 내용
+# A : best!!!
+# B : good!!
+# C : run!
+# D : slowly~
+# 나머지 문자들 : what?
+
+
+str = input()
+if str =='A':
+    print('best!!!')
+elif str =='B':
+    print('good!!')
+elif str =='C':
+    print('run!')
+elif str =='D':
+    print('slowly~')
+else:
+    print('what?')
