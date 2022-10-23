045f2103ea63e0b785b24269c7ec52eda548c17b
Saru <heo_dark@naver.com>
2022-10-23 15:31:04 +0900

    promise, async, await

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  
+  root: true,
+
   parserOption: {
     ecmaVersion: 2020,
   },
-  
+
   env: {
-    node: true
+    node: true,
   },
 
   extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
--- src/main4.js
+++ src/main4.js
@@ -1,12 +1,78 @@
 // @ts-check
 
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello, node.js');
-});
+/* eslint-disable no-new */
+/* eslint-disable no-console */
 
-const PORT = 4000;
-server.listen(PORT, () => {
-  console.log(`The server is listening at PORT : ${PORT}`);
-});
+function returnPromiseForTimeout() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Math.random());
+    }, 1000);
+  });
+}
+
+returnPromiseForTimeout()
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  })
+  .then((value) => {
+    console.log(value);
+    return returnPromiseForTimeout();
+  });
+
+returnPromiseForTimeout();
+
+/**
+ * @param {number} duration
+ */
+function sleep(duration) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, duration);
+  });
+}
+
+sleep(1000)
+  .then((value) => {
+    console.log(value);
+    return sleep(1000);
+  })
+  .then((value) => {
+    console.log(value);
+    return sleep(1000);
+  })
+  .then((value) => {
+    console.log(value);
+    return sleep(1000);
+  })
+  .then((value) => {
+    console.log(value);
+    return sleep(1000);
+  });
+
+returnPromiseForTimeout();
+
+// new Promise((resolve, reject) => {
+//   console.log('Inside promise');
+//   reject(new Error('First reject'));
+//   console.log('Before resolve');
+//   resolve('First resolve');
+//   console.log('After resolve');
+// })
+//   .then((value) => {
+//     console.log('Inside first then');
+//     console.log('value', value);
+//   })
+//   .catch((error) => {
+//     console.log('Error', error);
+//   });
