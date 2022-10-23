836b96a7cc3769671486478444733e0a456043ff
Saru <heo_dark@naver.com>
2022-10-23 13:59:01 +0900

    first commit -221023

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  
+  parserOption: {
+    ecmaVersion: 2020,
+  },
+  
+  env: {
+    node: true
+  },
+
+  extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
+};
---
+++ src/main1.js
@@ -0,0 +1,25 @@
+const ary = [1, 2, 3, 4, 5];
+const [head, ...rest] = ary;
+
+console.log(head, rest);
+
+const personalData = {
+  email: 'abc@def.com',
+  password: '****',
+};
+
+const publicData = {
+  nickname: 'foo',
+};
+
+const overrides = {
+  email: 'fff@fff.com',
+};
+
+const user = {
+  ...personalData,
+  ...overrides,
+  ...publicData,
+};
+
+console.log(user);
