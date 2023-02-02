f2bd8c94d9c7ac64837937280db745f7d11ae773
Saru <heo_dark@naver.com>
2023-02-02 22:44:48 +0900

    Python review 230202 first commit

---
+++ .idea/230202.py
@@ -0,0 +1,21 @@
+# 6001 - Hello 출력
+print('6001:', 'Hello');
+
+# 6002 - Hello World 를 출력한다.
+print('6002:', 'Hello World');
+
+# 6003 - Hello \n World를 출력한다.
+print('6003:', "Hello");
+print("World");
+
+# 6004 - 'Hello' 를 출력한다.
+print('6004:', "'Hello'");
+
+# 6005 - "Hello World" 를 출력한다.
+print('6005:', '"Hello World"');
+
+# 6005 - "Hello World" 를 출력한다.
+print('6006:', '"Hello World"');
+
+# 6006 - "!@#$%^&*()' 를 출력한다.
+print('6006:', '"!@#$%^&*()\'');
