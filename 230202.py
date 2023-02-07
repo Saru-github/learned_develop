84d788e8735b7e3b42f054bf9c3d577abac95803
Saru <heo_dark@naver.com>
2023-02-07 22:35:53 +0900

    Python review 230202 / 6001~6006, 6 sol

--- 230202.py
+++ 230202.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #2023년 2월 2일 목요일
 
 # 6001 - Hello 출력
@@ -19,26 +19,3 @@ print('6005:', '"Hello World"')
 
 # 6006 - "!@#$%^&*()' 를 출력한다.
 print('6006:', '"!@#$%^&*()\'')
-=======
-# 6001 - Hello 출력
-print('6001:', 'Hello');
-
-# 6002 - Hello World 를 출력한다.
-print('6002:', 'Hello World');
-
-# 6003 - Hello \n World를 출력한다.
-print('6003:', "Hello");
-print("World");
-
-# 6004 - 'Hello' 를 출력한다.
-print('6004:', "'Hello'");
-
-# 6005 - "Hello World" 를 출력한다.
-print('6005:', '"Hello World"');
-
-# 6005 - "Hello World" 를 출력한다.
-print('6006:', '"Hello World"');
-
-# 6006 - "!@#$%^&*()' 를 출력한다.
-print('6006:', '"!@#$%^&*()\'');
->>>>>>> origin/master
