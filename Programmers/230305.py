0159c15abb792742ad3964fe9668377cd15141af
Saru <heo_dark@naver.com>
2023-02-14 11:00:00 +0000

    Python review 230305 / Lv.0, 8 sol

--- Programmers/230305.py
+++ Programmers/230305.py
@@ -41,7 +41,44 @@ def solution(array):
     return sum
 # return str(array).count('7')
 
-print(solution([7, 77, 17]))
+# 120852 - 소인수분해
+# 소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다.
+# 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다.
+# 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(n):
+    answer = []
+    d = 2
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1
+    return answer
+
+# 120843 - 공 던지기
+# 머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다.
+# 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다.
+# 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때,
+# k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+
+def solution(numbers, k):
+    return numbers[2 * (k - 1) % len(numbers)]
+
+# 120894 - 영어가 싫어요
+# 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+# 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+def solution(numbers):
+    numArr = {'zero' : 0, 'one':1, 'two':2, 'three' : 3, 'four':4,
+                 'five':5,'six' : 6, 'seven' : 7, 'eight':8,'nine':9}
+    for x, y in numArr.items():
+        numbers = numbers.replace(x, str(y))
+    return int(numbers)
+
+print(solution("onetwothreefourfivesixseveneightnine"))
 
 
 
